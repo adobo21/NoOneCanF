28ec37bcec0049aad896a0ee933a7fc64666c1c5
adobo21 <122484591+adobo21@users.noreply.github.com>
2026-06-11 18:54:20 +0800

    Update tesssst.lua

--- tesssst.lua
+++ tesssst.lua
@@ -859,8 +859,8 @@ local LoadoutsModule = loadstring(game:HttpGet("https://raw.githubusercontent.co
 -- local LoadoutsModule = loadstring(game:HttpGet("https://pastebin.com/raw/"))()
 
 --LIVE EventModule
---local EventModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/XxMarDdEvsZXsWu69/bhubalt/refs/heads/main/bhubevent.lua"))()
-local EventModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/bhubAlt/bhub_alt/refs/heads/main/bhubevent.lua"))()
+local EventModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/XxMarDdEvsZXsWu69/bhubalt/refs/heads/main/bhubevent.lua"))()
+--local EventModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/bhubAlt/bhub_alt/refs/heads/main/bhubevent.lua"))()
 --DEV MODE2 
 -- local EventModule = loadstring(game:HttpGet("https://pastebin.com/raw/"))()
 
